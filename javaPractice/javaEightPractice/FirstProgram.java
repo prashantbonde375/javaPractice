5e6eda407a8309b95f076384b5246a51d97c961a
Prashant Bonde <prashant.bonde@veracity-india.com>
2025-05-16 23:13:12 +0530

    Java-Eight_Features :- First Commit ]for Java Eight Practice code.

---
+++ javaPractice/javaEightPractice/FirstProgram.java
@@ -0,0 +1,7 @@
+package javaPractice.javaEightPractice;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("JAva Eight Features IN Branch of JAva-Eight_Features");
+    }
+}
