028af30d7ffd79ac90d631ee944f67ea3d76c74b
Prashant Bonde <prashant.bonde@veracity-india.com>
2025-05-16 23:16:42 +0530

    First Project for java eight features.

--- javaPractice/javaEightPractice/FirstProgram.java
+++
@@ -1,7 +0,0 @@
-package javaPractice.javaEightPractice;
-
-public class FirstProgram {
-    public static void main(String[] args) {
-        System.out.println("JAva Eight Features IN Branch of JAva-Eight_Features");
-    }
-}
