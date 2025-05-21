9f5d1494be42dd98d3f36d06d51f08c97d33a850
Prashant Bonde <prashant.bonde@veracity-india.com>
2025-05-21 23:42:19 +0530

    refactor program

--- JavaEightFeatures/src/practice/Program_7.java
+++ JavaEightFeatures/src/practice/Program_7.java
@@ -1,10 +1,8 @@
 package practice;
 
-import java.security.KeyStore.Entry;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collector;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class Program_7 {
@@ -12,12 +10,12 @@ public class Program_7 {
         // divide given integer list into
         int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-        List<Integer> list = Arrays.stream(a).boxed().collect(Collectors.toList());
+        List<Integer> list = Arrays.stream(a).boxed().toList();
 
         List<List<Integer>> checkList = list.stream()
                 .collect(Collectors.groupingBy(x -> x % 2 == 0, Collectors.toList()))
-                .entrySet().stream().map(x -> x.getValue()).collect(Collectors.toList());
-
+                .entrySet().stream().map(Entry::getValue).toList();
+                //.map(x->x.getvalue()) we can use also
         System.out.println(checkList);
     }
 }
