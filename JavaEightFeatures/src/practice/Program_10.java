c677bdd0a5159835de371ed41584dd03286cf532
Prashant Bonde <prashant.bonde@veracity-india.com>
2025-05-22 02:39:11 +0530

    correct program

--- JavaEightFeatures/src/practice/Program_10.java
+++ JavaEightFeatures/src/practice/Program_10.java
@@ -6,9 +6,11 @@ public class Program_10 {
     public static void main(String[] args) {
         // Given an array, find the sum of unique elements
 
-        int[] a = { 9,8,7,6,5,4,3,2,1,1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int[] a = { 9, 8, 7, 5, 4, 3, 2, 1, 1, 2, 3, 7, 8, 9 };
 
-        Arrays.stream(a).distinct();
+        int array = Arrays.stream(a).distinct().sum();
+
+        System.out.println(array);
     }
 
 }
