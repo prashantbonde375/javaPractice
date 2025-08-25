0ec6c4b630349cf041f46604edc72df1fab580dd
STSARC <stsarcsubs@gmail.com>
2025-08-25 16:12:38 +0530

    PROGRAM-14 :- given string array print only digit from string array.

---
+++ JavaEightFeatures/src/august/Fourteen_Program.java
@@ -0,0 +1,15 @@
+package august;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Fourteen_Program {
+    public static void main(String[] args) {
+        //given string array print only digit from string array.
+
+        String[] str = { "abcd", "123", "xyz", "765", "987" };
+
+        List<Integer> list = Arrays.stream(str).filter(x -> x.matches("[0-9]+")).map(Integer::valueOf).toList();
+        System.out.println(list);
+    }
+}
