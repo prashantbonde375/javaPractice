13de688bc26a757c95dd2f3b67ce19460067aa9b
STSARC <stsarcsubs@gmail.com>
2025-08-25 16:01:31 +0530

    PROGRAM-2 :- given string, remove duplicates character from string.

---
+++ JavaEightFeatures/src/august/Two_Program.java
@@ -0,0 +1,13 @@
+package august;
+
+import java.util.Arrays;
+
+public class Two_Program {
+    public static void main(String[] args) {
+
+        //given string, remove duplicates character from string.
+        String str ="deadea";
+
+        Arrays.stream(str.split("")).distinct().forEach(System.out::println);
+    }
+}
